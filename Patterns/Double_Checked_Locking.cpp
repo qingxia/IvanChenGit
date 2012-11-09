fd6128c09bc6217db1fe041ad11afc479004f332
Ivan Chen <ivanchenhz@gmail.com>
2012-11-09 14:27:51 +0800

    Update some comments.

--- Patterns/Double_Checked_Locking.cpp
+++ Patterns/Double_Checked_Locking.cpp
@@ -1,8 +1,7 @@
 // Double checked locking optimization pattern:
 // Used to reduce the overhead of acquireing a lock by first testing
 // the locking criterion without actually acquiring the lock.
-// Only if the locking criterion check indicates that locking is 
-// required does the catual locking logic proceed.
+// Only does if the locking criterion check indicates that locking is required.
 
 // Used with Singleton pattern
 // With this optimization, we theoretical only need to acquire the lock once 
@@ -10,18 +9,18 @@
 T* GetInstance()
 {
     // First test whether need to acquire lock
-    if (0 == pInstance)
+    if (0 == m_pInstance)
     {
         Acquire_Lock();
 
         // Second test whether some other thread has alredy initialized
-        if (0 == pInstance)
+        if (0 == m_pInstance)
         {
-            pInstance = new Instance();
+            m_pInstance = new Instance();
         }
 
         Release_Lock();
     }
 
-    return pInstance;
+    return m_pInstance;
 }
